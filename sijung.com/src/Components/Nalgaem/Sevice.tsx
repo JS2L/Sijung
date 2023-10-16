9b2820f36e8414bc5ebbf210aeb250e201eebb4d
JS2L <dhwnatjr678@gmail.com>
2023-10-16 09:07:36 +0900

    231016 Morning Commit

--- sijung.com/src/Components/Nalgaem/Sevice.tsx
+++ sijung.com/src/Components/Nalgaem/Sevice.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 // 기상서비스 메인 내용
 function Service() {
-  return <Container>날갬란</Container>;
+  return <Container>날갬란이오</Container>;
 }
 
 export default Service;
